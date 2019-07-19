ee65854a5ab521b78e1c387156f5dc97bb304e77
安藤 優希 <ando-masaki@team-lab.com>
2019-07-19 12:31:29 +0900

    webpackの初期設定

--- src/scripts/midi.js
+++ src/scripts/midi.js
@@ -7,7 +7,6 @@ function getOrCreateContext() {
     oscillator.connect(context.destination);
   }
   return context;
-  
 }
 const list = document.getElementById('midi-list');
 const debugEl = document.getElementById('debug');
@@ -23,15 +22,54 @@ const eps = 0.01;
 
 // This is a tetris theme transposed from https://musescore.com/user/16693/scores/38133
 const tetris = [
-  [76, 4], [71, 8], [72, 8], [74, 4], [72, 8], [71, 8], [69, 4], [69, 8], [72, 8], [76, 4], [74, 8], [72, 8], [71, 4], [71, 8], [72, 8], [74, 4], [76, 4], [72, 4], [69, 4], [69, 4], [0,  4], [74, 3], [77, 8],[81, 4], [79, 8], [77, 8], [76, 3], [72, 8], [76, 4], [74, 8], [72, 8], [71, 4], [71, 8], [72, 8], [74, 4], [76, 4], [72, 4], [69, 4], [69, 4], [0, 4],
-]
+  [76, 4],
+  [71, 8],
+  [72, 8],
+  [74, 4],
+  [72, 8],
+  [71, 8],
+  [69, 4],
+  [69, 8],
+  [72, 8],
+  [76, 4],
+  [74, 8],
+  [72, 8],
+  [71, 4],
+  [71, 8],
+  [72, 8],
+  [74, 4],
+  [76, 4],
+  [72, 4],
+  [69, 4],
+  [69, 4],
+  [0, 4],
+  [74, 3],
+  [77, 8],
+  [81, 4],
+  [79, 8],
+  [77, 8],
+  [76, 3],
+  [72, 8],
+  [76, 4],
+  [74, 8],
+  [72, 8],
+  [71, 4],
+  [71, 8],
+  [72, 8],
+  [74, 4],
+  [76, 4],
+  [72, 4],
+  [69, 4],
+  [69, 4],
+  [0, 4]
+];
 
 function playTetris() {
   getOrCreateContext();
   oscillator.start(0);
   var time = context.currentTime + eps;
-  tetris.forEach(note => {
-    const freq = Math.pow(2, (note[0]-69)/12)*440;
+  tetris.forEach((note) => {
+    const freq = Math.pow(2, (note[0] - 69) / 12) * 440;
     console.log(time);
     oscillator.frequency.setTargetAtTime(0, time - eps, 0.001);
     oscillator.frequency.setTargetAtTime(freq, time, 0.001);
@@ -39,17 +77,25 @@ function playTetris() {
   });
 }
 
-document.getElementById('typesine').addEventListener('click', changeType.bind(null, 'sine'));
-document.getElementById('typesquare').addEventListener('click', changeType.bind(null, 'square'));
-document.getElementById('typesaw').addEventListener('click', changeType.bind(null, 'sawtooth'));
-document.getElementById('typetriangle').addEventListener('click', changeType.bind(null, 'triangle'));
+document
+  .getElementById('typesine')
+  .addEventListener('click', changeType.bind(null, 'sine'));
+document
+  .getElementById('typesquare')
+  .addEventListener('click', changeType.bind(null, 'square'));
+document
+  .getElementById('typesaw')
+  .addEventListener('click', changeType.bind(null, 'sawtooth'));
+document
+  .getElementById('typetriangle')
+  .addEventListener('click', changeType.bind(null, 'triangle'));
 
 document.getElementById('tetris').addEventListener('click', playTetris);
 
 function noteOn(midiNote) {
   console.log(midiNote);
   getOrCreateContext();
-  const freq = Math.pow(2, (midiNote-69)/12)*440;
+  const freq = Math.pow(2, (midiNote - 69) / 12) * 440;
   oscillator.frequency.setTargetAtTime(freq, context.currentTime, 0);
   if (!isStarted) {
     oscillator.start(0);
@@ -66,42 +112,40 @@ function noteOff() {
 function connectToDevice(device) {
   console.log('Connecting to device', device);
   device.onmidimessage = function(m) {
-    const [command, key, velocity] = m.data;
+    const [command, key] = m.data;
     if (command === 145) {
       debugEl.innerText = 'KEY UP: ' + key;
       noteOn(key);
-    } else if(command === 129) {
+    } else if (command === 129) {
       debugEl.innerText = 'KEY DOWN';
       noteOff();
     }
-  }
+  };
 }
 
 function replaceElements(inputs) {
-  while(list.firstChild) {
-    list.removeChild(list.firstChild)
+  while (list.firstChild) {
+    list.removeChild(list.firstChild);
   }
-  const elements = inputs.map(e => {
-        console.log(e);
-        const el = document.createElement('li')
-        el.innerText = `${e.name} (${e.manufacturer})`;
-        el.addEventListener('click', connectToDevice.bind(null, e));
-        return el;
-    });
+  const elements = inputs.map((e) => {
+    console.log(e);
+    const el = document.createElement('li');
+    el.innerText = `${e.name} (${e.manufacturer})`;
+    el.addEventListener('click', connectToDevice.bind(null, e));
+    return el;
+  });
 
-    elements.forEach(e => list.appendChild(e));
+  elements.forEach((e) => list.appendChild(e));
 }
 
-navigator.requestMIDIAccess()
-    .then(function(access) {
-      console.log('access', access);
-      replaceElements(Array.from(access.inputs.values()));
-      access.onstatechange = function(e) {
-        replaceElements(Array.from(this.inputs.values()));
-      }
-
-    })
- console.log(navigator);
+navigator.requestMIDIAccess().then(function(access) {
+  console.log('access', access);
+  replaceElements(Array.from(access.inputs.values()));
+  access.onstatechange = function() {
+    replaceElements(Array.from(this.inputs.values()));
+  };
+});
+console.log(navigator);
 
 // Below is keyboard emulation for C4-C5 q-i keys
 var emulatedKeys = {
@@ -112,8 +156,8 @@ var emulatedKeys = {
   t: 67,
   y: 69,
   u: 71,
-  i: 72,
-}
+  i: 72
+};
 
 document.addEventListener('keydown', function(e) {
   console.log(e);
---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const StyleLintPlugin = require('stylelint-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/scripts/midi.js',
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'eslint-loader'
+      },
+      {
+        // 拡張子 .js の場合
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: [
+          {
+            // Babel を利用する
+            loader: 'babel-loader',
+            // Babel のオプションを指定する
+            options: {
+              presets: [
+                // プリセットを指定することで、ES2018 を ES5 に変換
+                '@babel/preset-env',
+                // React の JSX を解釈
+                //'@babel/react'
+              ]
+            }
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(jpg|png)$/,
+        loader: 'url-loader',
+        options: { limit: 8192 }
+      }
+    ]
+  },
+  plugins: [
+    new UglifyJSPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/midi.html'
+    }),
+    new StyleLintPlugin({
+      configFile: '.stylelintrc'
+    })
+  ],
+  resolve: {
+    extensions: ['.js']
+  },
+  devServer: {
+    port: 3000,
+    historyApiFallback: true
+  }
+};
